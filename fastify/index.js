502880aa95e10c82b70d906c648454832b1d0dc6
Willie Le <williele99@gmail.com>
2020-06-13 17:58:02 +0700

    fixed pwa data group configure

--- fastify/index.js
+++ fastify/index.js
@@ -1,34 +1,41 @@
 const path = require("path");
-const fastifyMongooseAPI = require('fastify-mongoose-api');
-const fastifyFormbody = require('fastify-formbody');
+// const fastifyMongooseAPI = require("fastify-mongoose-api");
+// const fastifyFormbody = require("fastify-formbody");
 
-const fastify = require('fastify')({
-    logger: true
+const fastify = require("fastify")({
+  logger: true,
 });
 
-fastify.register(fastifyFormbody);
-fastify.register(require('fastify-cors'));
+// fastify.register(fastifyFormbody);
+fastify.register(require("fastify-cors"));
 
+const distDir = path.join(process.cwd(), "dist/volunt");
 
-fastify.register(require('./routes/route'),{prefix: '/api'})
-
-const distDir = "/dist/volunt";
-
-fastify.register(require('fastify-static'), {
-    root: path.join(process.cwd(), distDir),
-    prefix: '/'
+fastify.register(require("fastify-static"), {
+  root: distDir,
+  wildcard: false,
 });
 
-const address = "0.0.0.0"
-const port = 3000
+// fastify.register(require("./routes/route"), { prefix: "/api" });
+
+fastify.get("/api/user", (req, res) => {
+  res.send({ hello: "world" });
+});
+
+fastify.get("/*", (req, res) => {
+  res.sendFile("index.html", distDir);
+});
+
+const address = "0.0.0.0";
+const port = 3000;
 
 const app = async (port, address) => {
-    try{
-        await fastify.listen(port, address)
-    } catch(err){
-        fastify.log.error(err)
-        process.exit(1)
-    }
-}
+  try {
+    await fastify.listen(port, address);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
 
-app(port, address)
+app(port, address);
