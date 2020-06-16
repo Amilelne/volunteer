05c46d07ae41aa8437884c1de188de850a7c67b0
kai-ilki <kailam11223@gmail.com>
2020-06-16 21:32:19 +0800

    update

--- server/Backend/NCov/routes/g_user.js
+++ server/Backend/NCov/routes/g_user.js
@@ -12,6 +12,10 @@ var corsOptions = {
 
 }
 
+// --------------------------------------------------------------------------------------------------------------------
+// USER BEHAVIORS:
+// LOGIN, LOGOUT
+
 router.post('/login', cors(corsOptions),function(req, res) {
   // return volunteerList if success
   // Maximum return 10
@@ -28,10 +32,6 @@ router.post('/login', cors(corsOptions),function(req, res) {
           if(err2 == null){
             if(docs2.length!=0){
               var collection = db.get('volunteerList');
-              // collection.find({},{limit:10},function(err1,docs1){
-              // 	  console.log(docs1);
-              //     res.send(docs1);
-              // });
               collection.aggregate([
 				  { $limit: 10 },
 				  { $lookup: { from: 'userList', localField: 'username', foreignField: 'username', as: 'username' } }
@@ -53,6 +53,11 @@ router.get('/logout', cors(corsOptions),function(req, res) {
   res.send("logout");
  
 });
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// MANAGE DEMAND:
+// ADD, EDIT, DELETE, COMMENT
 
 
 
