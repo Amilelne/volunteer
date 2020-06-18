9cb65fa9c4e7bab37cbf2473730b4f2542d726e2
kai-ilki <kailam11223@gmail.com>
2020-06-18 14:05:45 +0800

    update

--- server/Backend/NCov/routes/g_user.js
+++ server/Backend/NCov/routes/g_user.js
@@ -25,29 +25,19 @@ router.post('/login', cors(corsOptions),function(req, res) {
   var db = req.db;
   var collection1 = db.get('userList');
   var collection2 = db.get('guserList');
-  collection1.find({"username":username},{},function(err,docs){
-    if(err == null){
-      if(docs.length!=0&&docs[0]['password']==password){
-          collection2.find({"username":username},{},function(err2,docs2){
-          if(err2 == null){
-            if(docs2.length!=0){
-              var collection = db.get('volunteerList');
-              collection.aggregate([
-				  { $limit: 10 },
-				  { $lookup: { from: 'userList', localField: 'username', foreignField: 'username', as: 'username' } }
-				],function(err1,docs1){
-	              	  console.log(docs1);
-	                  res.send(docs1);
-	           });
-            }else res.json("Login failure");
-          }else res.json(err);
-          
-
-      });
-    }else res.json("Login failure");
-  }else res.json(err);
-});
-});
+	let response = verify(req,username,password); 
+	console.log(response);
+    if(response){
+		var collection = db.get('volunteerList');
+	    collection.aggregate([
+			{ $limit: 10 },
+			{ $lookup: { from: 'userList', localField: 'username', foreignField: 'username', as: 'username' } }
+		],function(err1,docs1){
+		    console.log(docs1);
+		    res.send(docs1);
+		});
+		}else res.json("Login failure");
+	});
 
 router.get('/logout', cors(corsOptions),function(req, res) {
   res.send("logout");
@@ -59,10 +49,51 @@ router.get('/logout', cors(corsOptions),function(req, res) {
 // MANAGE DEMAND:
 // ADD, EDIT, DELETE, COMMENT
 
+router.post('/addDemand', cors(corsOptions),function(req, res){
+  var db = req.db;
+  var username = req.body.username;
+  var password = req.body.password;
+  var _id = req.params.photoid;
+  var collection = db.get('userList');
+  collection.find({"_id":userid},{},function(err,docs){
+    username = docs[0]['username'];
+    var collection1 = db.get('photoList');
+    collection1.findOneAndUpdate({_id:_id}, { $push: {"likedby":username}}, function (err1, result) {
+      res.json(
+          (err1 === null) ? { msg: result["likedby"] } : { msg: err }
+      );
+    });
+  });
+  
+  
+});
 
 
 
-
+async function verify(req,username,password){
+  var db = req.db;
+  var collection1 = db.get('userList');
+  var collection2 = db.get('guserList');
+  var res;
+  await collection1.find({"username":username},{}, function(err,docs){
+    if(err == null){
+    	console.log("1");
+      if(docs.length!=0&&docs[0]['password']==password){
+      	console.log("2");
+         collection2.find({"username":username},{},function(err2,docs2){
+          if(err2 == null){
+          	console.log("3");
+            if(docs2.length!=0){
+            	res = true;
+            }else { res = false};
+          }else { res = false};     
+      });
+    }else { res = false};
+  }else { res = false};
+});
+  console.log(res);
+  return res;
+}
 /*
  * Handle preflighted request
  */
