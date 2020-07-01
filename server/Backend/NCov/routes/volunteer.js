33a2b15c4b66ac40ba37b859d62a02108a0fce5d
kai-ilki <kailam11223@gmail.com>
2020-07-01 11:34:53 +0800

    Complete all fxs

--- server/Backend/NCov/routes/volunteer.js
+++ server/Backend/NCov/routes/volunteer.js
@@ -14,7 +14,7 @@ var corsOptions = {
 
 // --------------------------------------------------------------------------------------------------------------------
 // USER BEHAVIORS:
-// LOGIN, LOGOUT
+// LOGIN, LOGOUT, REGISTER
 
 router.post('/login', cors(corsOptions), async function (req, res) {
   // return demandList if success
@@ -43,7 +43,35 @@ router.get('/logout', cors(corsOptions), function (req, res) {
 
 });
 
+router.post('/register', cors(corsOptions), async function (req, res) {
+  // get info
+  var db = req.db;
+  var username = req.body.username;
+  var password = req.body.password;
+  var phone = req.body.phone;
+  var email = req.body.email;
+  
+  var collection = db.get('userList');
+    collection.find({
+      username: username
+    },function (err1, docs1) {
+      if(docs1.length==0){
+        path = __dirname+"/../public/uploads/" + username + "_cert.jpg";
+        req.pipe(fs.createWriteStream(path)).on('finish',function(){
+          collection.insert({username:username, password:password,phone:phone,email:email},function (err1, docs1) {
+            var collection1 = db.get('volunteerList');
+            collection1.insert({username:username, password:password},function (err1, docs1) {
+              res.json("success");
+            });       
+          });
+        });
+      }else{
+        res.json("Failure: username is used");
+      }
+    });
 
+
+});
 // --------------------------------------------------------------------------------------------------------------------
 // MANAGE NEED:
 // ADD, EDIT, VIEW, DELETE
@@ -169,6 +197,82 @@ router.delete('/deleteNeed', cors(corsOptions), async function (req, res) {
 
 });
 
+// --------------------------------------------------------------------------------------------------------------------
+// MANAGE DEMAND:
+// ACCEPT, FINISH
+
+router.post('/acceptDemand', cors(corsOptions), async function (req, res) {
+  // get info
+  var db = req.db;
+  var username = req.body.username;
+  var password = req.body.password;
+  var _id = req.body._id;
+
+
+  verify(req, username, password).then(() => {
+    setTimeout(function () {
+      if (response == true) {
+        var collection = db.get('demandList');
+        collection.update({
+            _id: _id,
+            state: "Waiting"
+          }, {
+            $set: {
+              vusername:username,
+              state: "Accepted"
+            }
+          },
+          function (err, result) {
+            if (result['nModified']!=0){
+              console.log(result);
+              res.json("Demand is accepted successfully");
+            }else{
+              res.json("Failure: Demand was accepted by other organization already.");
+            }
+          }
+        );
+      } else res.json("Authentication Failure");
+    }, 50);
+  });
+
+
+});
+
+router.post('/finishDemand', cors(corsOptions), async function (req, res) {
+  // get info
+  var db = req.db;
+  var username = req.body.username;
+  var password = req.body.password;
+  var _id = req.body._id;
+
+
+  verify(req, username, password).then(() => {
+    setTimeout(function () {
+      if (response == true) {
+        var collection = db.get('demandList');
+        collection.update({
+            _id: _id,
+            vusername: username,
+            state: "Accepted"
+          }, {
+            $set: {
+              state: "Finish"
+            }
+          },
+          function (err, result) {
+            if (result['nModified']!=0){
+              res.json("Demand is finished successfully");
+            }else{
+              res.json("Failure: Demand was not accepted by this username");
+            }
+          }
+        );
+      } else res.json("Authentication Failure");
+    }, 50);
+  });
+
+
+});
 
 // --------------------------------------------------------------------------------------------------------------------
 // Utilities:
